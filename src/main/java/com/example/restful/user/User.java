a85aff175c4f305ee50d528c271958e818d815c3
alwaysmoon <alwaysmoon09@gmail.com>
2020-08-13 00:41:50 +0900

    사용자 등록을 위한 API 구현

--- src/main/java/com/example/restful/user/User.java
+++ src/main/java/com/example/restful/user/User.java
@@ -4,10 +4,13 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+	
 	private Integer id;
 	private String name;
 	private Date date;
