6850b21658f231f347dda6b56f4093e8855809c1
alwaysmoon <alwaysmoon09@gmail.com>
2020-08-23 23:40:21 +0900

    유효성 체크를 위한 Validation API 사용

--- src/main/java/com/example/restful/user/User.java
+++ src/main/java/com/example/restful/user/User.java
@@ -2,6 +2,9 @@ package com.example.restful.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,9 @@ import lombok.NoArgsConstructor;
 public class User {
 	
 	private Integer id;
+	
+	@Size(min=2, message="Name은 2글자 이상 입력해주세요")
 	private String name;
+	@Past
 	private Date date;
 }
--- src/main/java/com/example/restful/user/UserController.java
+++ src/main/java/com/example/restful/user/UserController.java
@@ -3,6 +3,8 @@ package com.example.restful.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,7 +47,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<User> createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 		
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
